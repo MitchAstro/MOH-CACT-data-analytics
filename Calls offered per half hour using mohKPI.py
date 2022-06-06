2f9df24ee725b6adcb017764380ec589135bfd23
Mitchell James Bonham <46056696+MitchAstro@users.noreply.github.com>
2022-06-07 08:54:42 +1000

    Plots number of inbound calls per half hr, daily

--- Calls offered per half hour using mohKPI.py
+++ Calls offered per half hour using mohKPI.py
@@ -4,6 +4,7 @@ import mohKPI as mh
 
 #Main function controls the rest of the program  
 def main():
+    
     # Read in the queue metrics interval report
     df = mh.Clean_queue_metrics_int_report(r"C:\Genesys reporting\Call volumes per hour code\06-06-2022\All inbound queues half hour interval report.csv").return_clean_queue_metrics()
     # Plot the number of calls per half hour for each day of the report
